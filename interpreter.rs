f8121e505c0f123096fd35104bbebed91128c2c8
Ken Pratt <ken@kenpratt.net>
2014-07-11 21:31:24 -0700

    Changed interpreter to return values, not AST nodes.

--- interpreter.rs
+++ interpreter.rs
@@ -3,8 +3,16 @@ use parser::Node;
 
 use std::fmt;
 
-pub fn interpret(nodes: &Vec<Node>) -> Result<Vec<Node>, RuntimeError> {
+pub fn interpret(nodes: &Vec<Node>) -> Result<Vec<Value>, RuntimeError> {
     evaluate_nodes(nodes)
+}
+
+#[deriving(Show, PartialEq, Clone)]
+pub enum Value {
+    Integer(int),
+    Boolean(bool),
+    String(String),
+    List(Vec<Value>),
 }
 
 pub struct RuntimeError {
@@ -23,7 +31,7 @@ macro_rules! runtime_error(
     )
 )
 
-fn evaluate_nodes(nodes: &Vec<Node>) -> Result<Vec<Node>, RuntimeError> {
+fn evaluate_nodes(nodes: &Vec<Node>) -> Result<Vec<Value>, RuntimeError> {
     let mut results = Vec::new();
     for node in nodes.iter() {
         let res = try!(evaluate_node(node));
@@ -32,21 +40,23 @@ fn evaluate_nodes(nodes: &Vec<Node>) -> Result<Vec<Node>, RuntimeError> {
     Ok(results)
 }
 
-fn evaluate_node(node: &Node) -> Result<Node, RuntimeError> {
+fn evaluate_node(node: &Node) -> Result<Value, RuntimeError> {
     match node {
+        &parser::Integer(v) => Ok(Integer(v)),
+        &parser::Boolean(v) => Ok(Boolean(v)),
+        &parser::String(ref v) => Ok(String(v.clone())),
         &parser::List(ref vec) => {
             if vec.len() > 0 {
-                let evaluated = try!(evaluate_nodes(vec));
-                evaluate_expression(&evaluated)
+                evaluate_expression(vec)
             } else {
-                Ok(parser::List(vec![]))
+                Ok(List(vec![]))
             }
         },
-        _ => Ok(node.clone())
+        _ => runtime_error!("Can't evaluate node: {}", node) // Ok(node.clone())
     }
 }
 
-fn evaluate_expression(nodes: &Vec<Node>) -> Result<Node, RuntimeError> {
+fn evaluate_expression(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
     if nodes.len() == 0 {
         runtime_error!("Can't evaluate an empty expression: {}", nodes);
     }
@@ -60,26 +70,29 @@ fn evaluate_expression(nodes: &Vec<Node>) -> Result<Node, RuntimeError> {
                     }
                     let mut sum = 0;
                     for n in nodes.tailn(1).iter() {
-                        match *n {
-                            parser::Integer(x) => sum += x,
+                        let v = try!(evaluate_node(n));
+                        match v {
+                            Integer(x) => sum += x,
                             _ => runtime_error!("Unexpected node during +: {}", n)
                         };
                     };
-                    Ok(parser::Integer(sum))
+                    Ok(Integer(sum))
                 },
                 "-" => {
                     if nodes.len() != 3 {
                         runtime_error!("Must supply exactly two arguments to -: {}", nodes);
                     }
-                    let mut result = match *nodes.get(1) {
-                        parser::Integer(x) => x,
+                    let v1 = try!(evaluate_node(nodes.get(1)));
+                    let v2 = try!(evaluate_node(nodes.get(2)));
+                    let mut result = match v1 {
+                        Integer(x) => x,
                         _ => runtime_error!("Unexpected node during -: {}", nodes)
                     };
-                    result -= match *nodes.get(2) {
-                        parser::Integer(x) => x,
+                    result -= match v2 {
+                        Integer(x) => x,
                         _ => runtime_error!("Unexpected node during -: {}", nodes)
                     };
-                    Ok(parser::Integer(result))
+                    Ok(Integer(result))
                 },
                 _ => {
                     runtime_error!("Unknown function: {}", func);
