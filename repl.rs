36aa24480149851697994162ac6cf27b0720e36c
Ken Pratt <ken@kenpratt.net>
2014-07-30 01:11:35 -0700

    Improved handling of empty lines in REPL.

--- repl.rs
+++ repl.rs
@@ -13,14 +13,17 @@ fn prompt_for_input(prompt: &str) -> Option<String> {
         // wait for enter/CTRL-C/CTRL-D
         let raw = readline(prompt_c_str.as_ptr());
 
-        // add to shell history
-        add_history(raw);
-
         // parse into String and return
         let cs = CString::new(raw, true);
         if cs.is_null() {
             None
         } else {
+            // add to shell history unless it's an empty string
+            if cs.len() > 0 {
+                add_history(raw);
+            }
+
+            // return Option<String>
             cs.as_str().map(|s| s.to_str())
         }
     }
@@ -30,8 +33,10 @@ pub fn start(prompt: &str, f: |String| -> Result<String, String>) {
     loop {
         match prompt_for_input(prompt) {
             Some(input) => {
-                let result = f(input);
-                println!("{}", result.unwrap_or_else(|e| e));
+                if input.len() > 0 {
+                    let result = f(input);
+                    println!("{}", result.unwrap_or_else(|e| e));
+                }
             },
             None => return
         };
